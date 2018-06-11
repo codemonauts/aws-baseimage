c228065d8329b6ef336376edef6ce55e4c54a68d
Felix Breidenstein <felix@codemonauts.com>
2018-06-11 16:40:57 +0200

    common: Enable systemd-timesyncd to get NTP

--- cookbooks/common/recipes/default.rb
+++ cookbooks/common/recipes/default.rb
@@ -1,5 +1,7 @@
 apt_update 'update'
 
+# Install a set of default tools
+# Allows easier meintenace via ssh
 package 'tmux'
 package 'vim'
 package 'apt-transport-https'
@@ -8,3 +10,8 @@ package 'less'
 package 'ncdu'
 package 'psmisc'
 package 'tree'
+
+# Enable systemd-timesyncd to sync with NTP
+service 'systemd-timesyncd' do
+    action [:enable, :start]
+  end
