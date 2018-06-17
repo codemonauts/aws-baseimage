13fc66be2b0c7deeb981fb3964708ec89b6f7b8a
Felix Breidenstein <felix@codemonauts.com>
2018-06-17 23:42:58 +0200

    Fixed typo in test.sh script

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 set -eu
 
-echo "> Checcking packer.json for errors..."
+echo "> Checking packer.json for errors..."
 packer validate packer.json
 
-echo "> Checcking Chef cookbooks for errors..."
+echo "> Checking Chef cookbooks for errors..."
 # Ignore rule FC013 because file_cache_path threw errors and was disbaled
 foodcritic -f any -t "~FC013" ./cookbooks
