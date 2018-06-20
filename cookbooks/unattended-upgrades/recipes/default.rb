f70a883c1168bb5a2b2a2612100cb792c8cc0abf
Felix Breidenstein <felix@codemonauts.com>
2018-06-20 15:17:11 +0200

    unattended-upgrades: Disable apt-daily.service

--- cookbooks/unattended-upgrades/recipes/default.rb
+++ cookbooks/unattended-upgrades/recipes/default.rb
@@ -7,3 +7,13 @@ cookbook_file '/etc/apt/apt.conf.d/50unattended-upgrades' do
   mode '0644'
   action :create
 end
+
+# Disable the own unattented-upgrades timer/service by Ubuntu which where introduced in 16.04
+# These run on reboot, and conflict with our chef-solo run because apt is locked
+service 'apt-daily.service' do
+  action :disable
+end
+
+service 'apt-daily.timer' do
+  action :disable
+end
