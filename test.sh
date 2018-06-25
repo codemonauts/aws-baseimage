80fc94f9f3df2235c2117e003f5c1a9ddde45568
Felix Breidenstein <felix@codemonauts.com>
2018-06-25 18:18:14 +0200

    test.sh: Use local packer binary if available

--- test.sh
+++ test.sh
@@ -2,7 +2,14 @@
 set -eu
 
 echo "> Checking packer.json for errors..."
-packer validate packer.json
+# Check if we have a local packer executable in this folder
+# Otherwise use the system package
+if [ -f "packer" ]; then
+	PACKER_BIN="./packer"
+else
+	PACKER_BIN="packer"
+fi
+$PACKER_BIN validate packer.json
 
 echo "> Checking Chef cookbooks for errors..."
 # Ignore rule FC013 because file_cache_path threw errors and was disbaled
