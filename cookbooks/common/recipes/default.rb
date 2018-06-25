5796dee388bcfe211b8ffc0d39bcdeeac312ff58
Felix Breidenstein <felix@codemonauts.com>
2018-06-25 17:01:24 +0200

    common: Install DE and US with locale-gen

--- cookbooks/common/recipes/default.rb
+++ cookbooks/common/recipes/default.rb
@@ -14,4 +14,22 @@ package 'tree'
 # Enable systemd-timesyncd to sync with NTP
 service 'systemd-timesyncd' do
     action [:enable, :start]
-  end
+end
+
+# Install locales package and generate en_US and de_DE
+package 'locales'
+
+execute "locale-gen" do
+  command "locale-gen"
+  action :nothing
+end
+
+cookbook_file '/etc/locale.gen' do
+  source 'locale.gen'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+  notifies :run, "execute[locale-gen]", :immediate
+end
+
