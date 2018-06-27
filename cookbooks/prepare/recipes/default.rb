7b4d7a0f86e2645298a1b49caed98eb002f41e2b
Felix Breidenstein <felix@codemonauts.com>
2018-06-27 12:15:38 +0200

    prepare: Already provide the solo.rb config file for chef

--- cookbooks/prepare/recipes/default.rb
+++ cookbooks/prepare/recipes/default.rb
@@ -12,6 +12,20 @@ cookbook_file "/etc/systemd/system/chef-solo.service" do
   group "root"
 end
 
+directory '/var/chef' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+cookbook_file "/var/chef/solo.rb" do
+  source "solo.rb"
+  mode 0664
+  owner "root"
+  group "root"
+end
+
 service "chef-solo.service" do
   action [:enable]
 end
